35846d58a51f57f3af6ea05fc24917402740cfbf
mdcaceres <you@example.com>
2022-10-15 18:06:57 -0300

    PG2022-99 añadiendo componentes al module padre

--- dulceHeladeriaFrontend/src/app/app.module.ts
+++ dulceHeladeriaFrontend/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrarUsuarioComponent } from './administradores/components/registrar-usuario/registrar-usuario.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrarUsuarioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
