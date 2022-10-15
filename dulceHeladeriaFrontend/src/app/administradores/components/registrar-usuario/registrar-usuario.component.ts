c30618b5dfb72c50b4fec2d4bb3fd59a52b7ed68
mdcaceres <you@example.com>
2022-10-15 18:09:07 -0300

    PG2022-99 form group añadido

--- dulceHeladeriaFrontend/src/app/administradores/components/registrar-usuario/registrar-usuario.component.ts
+++ dulceHeladeriaFrontend/src/app/administradores/components/registrar-usuario/registrar-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-usuario',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar-usuario.component.css']
 })
 export class RegistrarUsuarioComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  form = new FormGroup({
+    nombre : new FormControl(),
+    apellido : new FormControl(),
+    dni : new FormControl(),
+    email : new FormControl(),
+    contraseña : new FormControl(),
+    confirmarContraseña : new FormControl(),
+    rol : new FormControl()
+  })
 
 }
