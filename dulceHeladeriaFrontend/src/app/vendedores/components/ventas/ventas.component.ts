80150421b591f8a8008914aefaa98403755d528e
Jeremias Quevedo <112958@tecnicatura.frc.utn.edu.ar>
2022-11-03 18:17:00 -0300

    PG2022-175 se conecto con api registrar venta y consultar productos

--- dulceHeladeriaFrontend/src/app/vendedores/components/ventas/ventas.component.ts
+++ dulceHeladeriaFrontend/src/app/vendedores/components/ventas/ventas.component.ts
@@ -1,167 +1,182 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Cliente } from '../../interfaces/cliente-interface';
 import { dtoNuevaVenta } from '../../interfaces/dtoVenta';
-import { gustos } from '../../interfaces/gustos';
-import { productos } from '../../interfaces/productos';
 import { NuevaVentaService } from '../../services/nueva-venta.service';
 import { ClientesComponent } from '../clientes/clientes.component';
-import {formatDate} from '@angular/common';
+import { ProductosService } from '../../services/productos.service';
+import { Producto } from '../../interfaces/producto';
+import { Articulo } from '../../interfaces/articulo';
+import { productoResponse } from '../../interfaces/productoResponse';
+import { VentaRequestDto } from '../../interfaces/ventaRequestDto';
 
 declare var window: any;
 
 @Component({
   selector: 'app-ventas',
   templateUrl: './ventas.component.html',
-  styleUrls: ['./ventas.component.css']
+  styleUrls: ['./ventas.component.css'],
 })
-export class VentasComponent implements OnInit, OnDestroy , AfterViewInit{
+export class VentasComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild(ClientesComponent) clientescomp: any;
 
-  cliente: Cliente;
-  gustos: gustos[] = [{ nombre: 'chocolate' }, { nombre: 'menta granizada' }, { nombre: 'vainilla' }, { nombre: 'frutilla' }, { nombre: 'DDL'}];
-  gustosSelected: gustos[] = [];
-  contadorGustos: number=0;
-  ventas: dtoNuevaVenta;
-  Nuevoproducto: productos={nombre:'',precio: 0, cantidad: 1, gustosS:[]};
+  nuevaFactura: dtoNuevaVenta = {};
+  venta: VentaRequestDto = {} as VentaRequestDto;
+  productosVenta: Producto[] = [];
+  total: number = 0;
   formModal: any;
-  selected: productos= this.Nuevoproducto;
-  nuevaVenta: dtoNuevaVenta = {total: 0, producto : []};
-  //arrayTabla: productos[] = []
-
-  arrayCard: productos[] = [{ nombre: 'helado 1KG', precio: 1000, cantidadMaxGustos: 4, imagen: "https://chio.com.ar/tienda/pehuajo/136-home_default/helado-artesanal-x-kilo.jpg",cantidad:1 },
-  { nombre: 'helado 1/2kg', precio: 700, cantidadMaxGustos: 3, imagen: "https://chio.com.ar/tienda/pehuajo/136-home_default/helado-artesanal-x-kilo.jpg",cantidad:1 },
-  { nombre: 'helado 1/4kg', precio: 500, cantidadMaxGustos: 2, imagen: "https://chio.com.ar/tienda/pehuajo/136-home_default/helado-artesanal-x-kilo.jpg",cantidad:1 }]
+  cliente: Cliente;
+  artSeleccionados: Articulo[] = [];
+  prodSeleccionado: Producto = {} as Producto;
+  productos: Producto[] = [];
+  imagen: string =
+    'https://chio.com.ar/tienda/pehuajo/136-home_default/helado-artesanal-x-kilo.jpg';
 
   private subscription = new Subscription();
-  ngAfterViewInit() {
-    //this.cliente = this.clientescomp.cliente;
-  }
-  //gustosForm = new FormGroup({
-    //gustos: new FormControl('-1', Validators.required),
-    //cantidad: new FormControl('',Validators.required),
-  //});
-
-  // ventaForm = new FormGroup({
-  //   nombre: new FormControl('', Validators.required),
-  //   precio: new FormControl(''),
-  //   cantidad: new FormControl(Validators.min(1)),
-  // });
+  ngAfterViewInit() {}
 
   constructor(
-    private ventaService: NuevaVentaService, private router: Router) { }
+    private ventaService: NuevaVentaService,
+    private productoService: ProductosService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.formModal = new window.bootstrap.Modal(
       document.getElementById('cantidades')
     );
+
+    this.cargarProductos();
   }
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
-  guardar() {
-    console.log(this.nuevaVenta)
-    this.subscription.add(this.ventaService.setVenta(this.nuevaVenta));
-    this.router.navigateByUrl('/vendedor/factura')
-    //this.subscription.add(
-    //  this.ventaService.registrarVenta(this.ventas).subscribe({
-    //    next: () => {
-    //      alert('exito')
-    //    },
-    //    error: () => {
-    //      alert('error al guardar la venta');
-     //   }
-     // })
-    //);
+  cargarProductos() {
+    this.subscription.add(
+      this.productoService.getProductos().subscribe({
+        next: (resp: productoResponse[]) => {
+          resp.forEach((element) => {
+            let prod: Producto = {
+              id: element.id,
+              nombre: element.name,
+              precio: element.price,
+              articulos: element.items.map((x) => {
+                return { id: x.id, nombre: x.name, seleccionado: false };
+              }),
+              cantMaxArticulos: element.maxItemAmount,
+            };
+            this.productos.push(prod);
+          });
+        },
+        error: (err) => {
+          console.log(err.error);
+        },
+      })
+    );
   }
 
+  registrarVenta() {
 
+    this.nuevaFactura = {
+      Cliente: this.cliente,
+      fecha: Date.now.toString(),
+      producto: this.productosVenta.map(x => {return {cantidad: x.cantidad!, nombre: x.nombre, precio: x.precio}})
+    }
 
-  openModal(producto: productos) {
+    this.subscription.add(this.ventaService.setVenta(this.nuevaFactura));
+
+    this.venta = {
+      clientId: this.cliente.id!,
+      clientName: this.cliente!.businessName,
+      paymentMethod: 1,
+      details: this.productosVenta.map((x) => {
+        return {
+          productId: x.id,
+          productName: x.nombre,
+          amount: x.cantidad!,
+          salePrice: x.precio,
+          productDetail: x.articulos.map((a) => {
+            return { id: a.id, name: a.nombre };
+          }),
+        };
+      }),
+    };
+
+    console.log(this.venta)
+
+    this.subscription.add(
+      this.ventaService.registrarVenta(this.venta).subscribe({
+        next: () => {
+          alert('exito');
+          this.router.navigateByUrl('/vendedor/factura');
+        },
+        error: () => {
+          alert('error al guardar la venta');
+        },
+      })
+    );
+
+    
+  }
+
+  openModal(producto: Producto) {
     this.formModal.show();
-    this.selected = producto
+    this.prodSeleccionado = producto;
   }
 
-  total: number=0;
   totalPrecio() {
-    this.total=0;
-    this.nuevaVenta.producto!.forEach(producto => {
-      this.total += (producto.cantidad! * producto.precio!);
+    this.productosVenta.forEach((producto) => {
+      this.total += producto.cantidad! * producto.precio!;
     });
   }
+  cargarGustos(articulo: Articulo) {
+    if (
+      this.artSeleccionados.includes(articulo) ||
+      this.artSeleccionados.length >= this.prodSeleccionado.cantMaxArticulos
+    ) {
+      return;
+    }
+    articulo.seleccionado = true;
+    this.artSeleccionados.push(articulo);
+  }
 
-cargarGustos(gusto: gustos, cantMax: number){
-  if(this.gustosSelected.includes(gusto) || this.gustosSelected.length>=cantMax){
-    return
+  quitarGustos(articulo: Articulo) {
+    this.artSeleccionados = this.artSeleccionados.filter(
+      (x) => x.id !== articulo.id
+    );
+    articulo.seleccionado = false;
   }
-  this.gustosSelected.push(gusto);
-}
-
-quitarGustos(gusto: gustos){
-  const id=this.gustosSelected.indexOf(gusto);
-  if(id != null){
-    this.gustosSelected.splice(id,1);
+  EliminarProducto(prod: Producto) {
+    this.productosVenta = this.productosVenta.filter((x) => x.id != prod.id);
+    this.total -= prod.precio;
   }
-  
-}
-EliminarProducto(prod: productos){
-  const id=this.nuevaVenta.producto!.indexOf(prod);
-  if(id != null){
-    this.nuevaVenta.producto!.splice(id,1);
-  }
-  
-}
 
   cerrarModal() {
     this.formModal.hide();
   }
 
-  registrarProducto(producto: productos) {
-      this.Nuevoproducto.gustosS=this.gustosSelected;
-      this.Nuevoproducto.cantidad=this.selected.cantidad;
-      this.Nuevoproducto.nombre=this.selected.nombre;
-      this.Nuevoproducto.precio= this.selected.precio!;
-      this.nuevaVenta.producto!.push(this.Nuevoproducto);
-      this.nuevaVenta.fecha = new Date().toLocaleDateString();
-      this.nuevaVenta.total=this.total;
-      this.nuevaVenta.Cliente= this.clientescomp.cliente;
-      //cambiar ID de venta
-      this.nuevaVenta.id=1;
-      this.totalPrecio();
-      this.cerrarModal();
-      this.gustosSelected=[];
-      //this.cantidad=0;
-      this.Nuevoproducto={nombre:'',precio: 0, cantidad: 0, gustosS:[]};
+  agregarProducto(prodSeleccionado: Producto) {
+    this.productosVenta.push(prodSeleccionado);
+    this.totalPrecio();
+    this.cerrarModal();
+    this.prodSeleccionado = {} as Producto;
+    this.artSeleccionados = [];
+    this.cliente = this.clientescomp.clienteSelected;
+  }
 
+  esVentaValida(): boolean {
+    if (this.cliente && this.productosVenta) {
+      return true;
+    } else {
+      return false;
     }
   }
-  // registrarVenta() {
-  //   if (this.formNuevo.valid) {
-  //     this.ventas = this.formNuevo.value as dtoNuevaVenta;
-  //     console.log(this.ventas);
-
-  //     if (this.formNuevo.controls.tipoGustos.value) {
-  //       this.ventas.producto = parseInt(
-  //         this.formNuevo.controls.tipoGustos.value
-  //       );
-  //     }
-
-  //     this.subscription.add(
-  //       this.ventaService.registrarVenta(this.ventas).subscribe({
-  //         next: (resp: any) => {
-  //           console.log(resp);
-  //           alert("guardado exitoso!")
-  //         },
-  //         error: () => {
-  //           alert('error');
-  //         },
-  //       })
-  //     );
-  //   }
-
-  //   this.cerrarModal();
-  //}
-
-//}
+}
---
+++ dulceHeladeriaFrontend/src/app/vendedores/services/productos.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { productoResponse } from '../interfaces/productoResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  constructor(private http: HttpClient) { }
+  getProductos(): Observable<productoResponse[]> {
+    return this.http.get<productoResponse[]>(`https://localhost:5001/api/product`);
+  }
+}
