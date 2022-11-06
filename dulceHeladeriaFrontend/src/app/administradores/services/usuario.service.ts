c61e2b806e49e9d6b2ecf5ab3ad8a2a4873bbf4a
Jeremias Quevedo <112958@tecnicatura.frc.utn.edu.ar>
2022-11-06 01:27:17 -0300

    PG2022-189 consultar usuarios y arreglos

--- dulceHeladeriaFrontend/src/app/administradores/services/usuario.service.ts
+++ dulceHeladeriaFrontend/src/app/administradores/services/usuario.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { usuario } from '../interfaces/Usuario';
+import { UsuarioResponse } from '../interfaces/usuarioResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,16 @@ export class UsuarioService {
 
   constructor(private http:HttpClient) { }
 
+  private jwt: string =localStorage.getItem('token')!
+
+  private _options = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'true', 'Authorization' : 'Bearer '+ this.jwt }),
+  };
+
   create(usuario:usuario): Observable<any>{
     return this.http.post<any>('https://localhost:5001/api/User/Register',usuario); 
+  }
+  getUsers(): Observable<UsuarioResponse[]>{
+    return this.http.get<UsuarioResponse[]>("https://localhost:5001/api/user",this._options)
   }
 }
