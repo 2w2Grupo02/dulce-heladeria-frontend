bd6cb83d9acd773a2aa17d1f617faa7b141403b4
mdcaceres <you@example.com>
2022-11-13 17:20:27 -0300

    reportes stock obtener reportes del backend

--- dulceHeladeriaFrontend/src/app/administradores/components/reporte-stock/lista-movimientos/lista-movimientos.component.ts
+++ dulceHeladeriaFrontend/src/app/administradores/components/reporte-stock/lista-movimientos/lista-movimientos.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Articulos } from 'src/app/administradores/interfaces/articulos';
+import { movimiento } from 'src/app/administradores/interfaces/Movimiento';
 import { ArticulosService } from 'src/app/administradores/services/articulos.service';
 import { MovimientosService } from 'src/app/administradores/services/movimientos.service';
 import { RangeService } from 'src/app/administradores/services/range.service';
+import { range } from 'src/app/administradores/interfaces/range';
 
 @Component({
   selector: 'app-lista-movimientos',
@@ -12,40 +15,76 @@ import { RangeService } from 'src/app/administradores/services/range.service';
 export class ListaMovimientosComponent implements OnInit {
 
 
-movimientos: any
-selectValue: any;
-items: Articulos[];
+  movimientos: movimiento[]
+  items: Articulos[]; 
+  myForm : FormGroup; 
 
-  constructor(private movimientoServie: MovimientosService,
+  constructor(
+    private fb:FormBuilder,
+    private movimientoServie: MovimientosService,
     private articuloService: ArticulosService,
-     private rangeService: RangeService) { }
+    private rangeService: RangeService
+    ) { }
 
   ngOnInit(): void {
+    this.getMovimientos();
     this.getItems(); 
+
+    this.myForm = this.fb.group({
+        item : [,]
+    })
+
+    this.onChangeItem(); 
+    this.rangeSub();
   }
 
   rangeSub(){
-    this.rangeService.rangeEmit().subscribe(
-      //next: () => {} 
-    )
+    this.rangeService.rangeEmit().subscribe({
+      next: (range:range) => {
+        this.movimientoServie.getMovimietosByDates(range.start!,range.end!).subscribe({
+          next : (resp: movimiento[]) => {
+            this.movimientos = resp; 
+            console.log("movimientos por fecha")
+            console.log(this.movimientos); 
+          },
+          error : () => {alert("error al obetener movimientos por fecha")}
+        })
+      }
+    })
   }
 
-  onChangeItem($event: Event) {
-    throw new Error('Method not implemented.');
-    }
+  onChangeItem() {
+    this.myForm.get("item")!.valueChanges.subscribe(x => {
+      this.movimientoServie.getMovimietosByItem(x).subscribe({
+        next : (res : movimiento[])  => {
+          this.movimientos = res; 
+          console.log("movimientos por items")
+          console.log(x); 
+          console.log(this.movimientos); 
+        }, 
+        error : (e) => {console.error(e)}
+      })
+    })
+  }
 
   getItems(){
     this.articuloService.getAll().subscribe({
       next: (resp) => {
         this.items = resp; 
-        console.log(resp); 
-        console.log(this.items);
       }
     })
   }
 
   getMovimientos(){
-    this.movimientoServie.getMovimietos().subscribe({});
+    this.movimientoServie.getMovimietos().subscribe({
+      next : (res : movimiento[])  => {
+        this.movimientos = res; 
+        console.log("movimientos sin filtro")
+        console.log(this.movimientos);
+        console.log("fin movimientos")
+      }, 
+      error : (e) => {console.error(e)}
+    });
   }
 
 }
